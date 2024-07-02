d7109f0e030be951f6b380dfa6810fdd088ec374
Eka Alifakih <eoffice.bpdlh@gmail.com>
2024-07-02 15:15:55 +0700

    CRUD jenis kelompok masyarakat
    Penambahan view fitur jenis kelompok masyarakat

---
+++ resources/views/pages/akseslh/jenis-kelompok-masyarakat/index.blade.php
@@ -0,0 +1,52 @@
+@extends('layouts.app')
+
+@section('title', 'Jenis Kelompok Masyarakat')
+
+@section('script')
+<script src="{{asset('app/build/akseslh_jenis_kelompok_masyarakat.js')}}" type="text/javascript"></script>
+@endsection
+
+@section('content')
+<!-- Page-Title -->
+<div class="row">
+    <div class="col-sm-12">
+        <h4 class="pull-left page-title">Daftar Jenis Kelompok Masyarakat</h4>
+        <ol class="breadcrumb pull-right">
+            <li><a href="#">Data Master</a></li>
+            <li class="active">Daftar Jenis Kelompok Masyarakat</li>
+        </ol>
+    </div>
+</div>
+
+<div class="row">
+    <div class="col-md-12">
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <h3 class="panel-title">Daftar Jenis Kelompok Masyarakat</h3>
+            </div>
+            <div class="panel-body">
+                <div class="row">
+                    <div class="col-md-12 col-sm-12 col-xs-12">
+                        <table id="dt_jenis_kelompok_masyarakat" class="table table-striped table-bordered">
+                            <thead>
+                                <tr>
+                                    <th>No</th>
+                                    <th>Jenis Kelompok Masyarakat</th>
+                                    <th>Short ID</th>
+                                    <th>Created at</th>
+                                    <th>Updated at</th>
+                                    <th></th>
+                                </tr>
+                            </thead>
+
+                        </table>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</div>
+<!-- End Row -->
+@endsection
