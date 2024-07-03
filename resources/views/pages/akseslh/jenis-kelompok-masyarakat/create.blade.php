9f73982268e5f63d05e58091ae20302305d8bf79
Eka Alifakih <eoffice.bpdlh@gmail.com>
2024-07-03 14:45:07 +0700

    Update Jenis Kelompok Masyrakat
    update jenis kelompok masyarakat di create, edit, index

--- resources/views/pages/akseslh/jenis-kelompok-masyarakat/create.blade.php
+++ resources/views/pages/akseslh/jenis-kelompok-masyarakat/create.blade.php
@@ -6,7 +6,7 @@
 <!-- Page-Title -->
 <div class="row">
     <div class="col-sm-12">
-        <h4 class="pull-left page-title">Pengelolaan Data Jenis Kelompok Masyarakat</h4>
+        <h4 class="pull-left page-title">KELOLA DATA JENIS KELOMPOK MASYARAKAT</h4>
         <ol class="breadcrumb pull-right">
             <li><a href="#">Data Master</a></li>
             <li><a href="#">Forms</a></li>
@@ -19,22 +19,27 @@
 <div class="row">
     <!-- Basic example -->
     <div class="col-md-6">
-        <div class="panel panel-default">
+        <div class="panel panel-primary">
             <div class="panel-heading">
                 <h3 class="panel-title">Pengelolaan Data Jenis Kelompok Masyarakat</h3>
             </div>
             <div class="panel-body">
-                <form role="form" action="{{ route('jenis-kelompok-masyarakat.store') }}" method="POST">
+                <form role="form" action="#" method="POST">
                     @csrf
-                    <div class="form-group @error('jenis_kelompok_masyarakat') has-error @enderror">
-                        <label for="jenis_kelompok_masyarakat">Jenis Kelompok Masyarakat <span class="text-danger">*</span></label>
-                        <input type="text" class="form-control" id="jenis_kelompok_masyarakat" name="jenis_kelompok_masyarakat" placeholder="Jenis Kelompok Masyarakat">
-                        <input type="number" class="form-control" min=0 id="short_id" name="short_id" default=0>
+                    <div class="form-group @error('jenis_kelompok_masyarakat') has-error @enderror row">
+                        <div class="col-sm-12">
+                            <label for="jenis_kelompok_masyarakat">Jenis Kelompok Masyarakat <span class="text-danger">*</span></label>
+                            <input type="text" class="form-control" id="jenis_kelompok_masyarakat" name="jenis_kelompok_masyarakat" placeholder="Jenis Kelompok Masyarakat">
+                        </div>
+                        <div class="col-sm-12">
+                            <label for="short_id">Nomor Urut<span class="text-danger">*</span></label>
+                            <input type="number" class="form-control" min=0 id="short_id" name="short_id" default=0>
+                        </div>
                         @error('jenis_kelompok_masyarakat')
                         {{ $message }}
                         @enderror
                     </div>
-                    <button type="submit" class="btn btn-purple waves-effect waves-light">Submit</button>
+                    <button type="submit" class="btn btn-primary waves-effect waves-light">Submit</button>
                 </form>
             </div><!-- panel-body -->
         </div> <!-- panel -->
