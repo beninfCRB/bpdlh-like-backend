532dcef0291114a193d8bf3b4e54b90a92c07cd0
Rifqi Arnoldy <rifqiarnoldy@gmail.com>
2024-08-05 13:42:31 +0700

    Change Register API

--- app/Http/Controllers/Authapi/RegisterController.php
+++ app/Http/Controllers/Authapi/RegisterController.php
@@ -3,17 +3,18 @@
 namespace App\Http\Controllers\Authapi;
 
 use Carbon\Carbon;
+use App\Models\UserAkseslh;
 use Illuminate\Support\Str;
 use Illuminate\Http\Request;
+use App\Models\UserEksternal;
+use App\Services\EmailPhpService;
 use Illuminate\Support\Facades\Hash;
 use App\Http\Controllers\ApiController;
+use App\Models\DataPicKelompokMasyarakat;
+use Illuminate\Support\Facades\Validator;
 use App\Notifications\RegisterNotification;
 use Illuminate\Support\Facades\Notification;
 use App\Http\Requests\Authapi\RegisterRequest;
-use App\Models\DataPicKelompokMasyarakat;
-use App\Models\UserAkseslh;
-use App\Models\UserEksternal;
-use App\Services\EmailPhpService;
 
 class RegisterController extends ApiController
 {
@@ -31,27 +32,50 @@ class RegisterController extends ApiController
 
     public function register(RegisterRequest $request): \Illuminate\Http\JsonResponse
     {
+        $validator = Validator::make($request->all(), [
+            'kelompok_masyarakat_id'      => 'required|exists:kelompok_masyarakats,id',
+            'email_pic'                   => 'required|email',
+            'nama_pic'                    => 'required|string',
+            'jenis_identitas_pic'         => 'required',
+            'nomor_identitas_pic'         => 'required',
+            'nohp_pic'                    => 'required'
+        ], [
+            'kelompok_masyarakat_id.exists' => 'User not found'
+        ]);
+
+        if ($validator->fails()) {
+            # code...
+            return $this->sendError(null, $validator->getMessageBag(), 422);
+        }
+
+        // Get all input
+        $input = $validator->validated();
+
         // Make default password for first login
         $default_password =
             crypt($request->email_pic . Carbon::now()->format('d M Y H:i:s'), $request->email_pic);
 
-        // Get all input
-        $input = $request->all();
-
         // Begin db transaction
         \DB::beginTransaction();
+
         try {
 
-            $user = DataPicKelompokMasyarakat::where([
-                'email_pic'              => $input['email_pic'],
-            ])
-                ->orWhere([
-                    'nohp_pic' => $input['nohp_pic']
-                ])
-                ->first();
+            $user = DataPicKelompokMasyarakat::where('kelompok_masyarakat_id', $input['kelompok_masyarakat_id'])
+                ->where(function ($query) use ($input) {
+                    $query
+                        ->where('nohp_pic', $input['nohp_pic'])
+                        ->orWhere('email_pic', $input['email_pic']);
+                })->first();
 
             if ($user) {
+
+                if ($user->user_akseslh->status_user == 'ACTIVE') return $this->sendError(null, 'Already active');
+
                 // Change user status to active
+                $user->email = $request->email_pic;
+                $user->save();
+
+                $user->user_akseslh->email          = $request->email_pic;
                 $user->user_akseslh->password       = Hash::make($default_password);
                 $user->user_akseslh->status_user    = 'ACTIVE';
                 $user->user_akseslh->save();
@@ -65,6 +89,7 @@ class RegisterController extends ApiController
 
                 // Commit Change
                 \DB::commit();
+
                 // Return token to frontend
                 return $this->sendSuccess([
                     'token'                     => $token,
@@ -76,7 +101,7 @@ class RegisterController extends ApiController
             } else {
 
                 \DB::rollBack(); // rollback the changes
-                return $this->sendError(null, "User not found");
+                return $this->sendError(null, "Anda belum memenuhi persyaratan untuk mendaftar. Untuk informasi lebih lanjut email layanandanamasyarakat@bpdlh.id");
             }
         } catch (\Throwable $th) {
 
