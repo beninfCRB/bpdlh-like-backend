b8f6065bd225ebf815e404d3dc043cd351fa2dca
Rifqi Arnoldy <rifqiarnoldy@gmail.com>
2024-09-25 13:38:44 +0700

    .

--- app/Services/Akseslh/ValidasiPengajuanKegiatanService.php
+++ app/Services/Akseslh/ValidasiPengajuanKegiatanService.php
@@ -253,26 +253,6 @@ class ValidasiPengajuanKegiatanService extends AppService implements AppServiceI
             $total += ($items->qty * $items->harga_unit);
         }
 
-        $dataSend = array(
-            'nomor_pengajuan'           => $read->nomor_pengajuan,
-            'keterangan'                => 'Disetujui',
-            'status'                    => '3',
-            'judul_pengajuan_kegiatan'  => $read->judul_pengajuan_kegiatan,
-            'total'                     => $total,
-            'nama_pic'                  => $read->user_akseslh->data_pic_kelompok_masyarakat->nama_pic,
-            'kelompok_masyarakat'       => $read->user_akseslh->data_pic_kelompok_masyarakat->kelompok_masyarakat->kelompok_masyarakat,
-        );
-
-        $this->emailService->pengajuanKegiatanDiterima(
-            $read->user_akseslh,
-            'Pemberitahuan Persetujuan Pengajuan Proposal Akses Dana Layanan Masyarakat untuk Lingkungan',
-            $dataSend,
-            null,
-            'mail.pengajuan-kegiatan-diterima'
-        );
-
-        dd($read->judul_pengajuan_kegiatan);
-
         \DB::beginTransaction();
 
         try {
