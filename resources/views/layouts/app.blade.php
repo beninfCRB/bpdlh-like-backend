c7e515030c8b3fb9f60a184c7773a6f614ab6b75
Rifqi Arnoldy <rifqiarnoldy@gmail.com>
2024-10-11 15:02:46 +0700

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -115,7 +115,7 @@
     <script src="{{ asset('assets/select2/select2.min.js') }}" type="text/javascript"></script>
 
     {{-- Dayjs --}}
-    <script src="{{ asset('assets/dayjs/dayjs.min.js') }}"></script>
+    <script src="{{ asset('assets/dayjs/package/dayjs.min.js') }}"></script>
 
     {{-- Mix --}}
     <script src="{{ mix('app/build/app.js') }}"></script>
