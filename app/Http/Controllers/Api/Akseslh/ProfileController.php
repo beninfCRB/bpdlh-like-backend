958e528c6592e7b58b06727c74777df6f08e3762
Rifqi Arnoldy <rifqiarnoldy@gmail.com>
2024-11-22 21:05:31 +0700

    Add Profile Endpoint

---
+++ app/Http/Controllers/Api/Akseslh/ProfileController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers\Api\Akseslh;
+
+use App\Http\Controllers\ApiController;
+use App\Http\Controllers\Controller;
+use App\Services\Akseslh\ProfileService;
+use Illuminate\Http\Request;
+
+class ProfileController extends ApiController
+{
+    protected $profileService;
+
+    public function __construct(
+        ProfileService $profileService,
+        Request $request
+    ) {
+        $this->profileService    =   $profileService;
+        parent::__construct($request);
+    }
+
+    public function index(Request $request): \Illuminate\Http\JsonResponse
+    {
+        $result = $this->profileService->apiGetAll();
+
+        try {
+            if ($result->success) {
+                return $this->sendSuccess($result->data, $result->message, $result->code);
+            }
+
+            return $this->sendError($result->data, $result->message, $result->code);
+        } catch (Exception $exception) {
+            return $this->sendError($exception->getMessage(), "", 500);
+        }
+    }
+
+    public function show($id)
+    {
+        $result = $this->profileService->show($id);
+
+        try {
+            if ($result->success) {
+                return $this->sendSuccess($result->data, $result->message, $result->code);
+            }
+
+            return $this->sendError($result->data, $result->message, $result->code);
+        } catch (Exception $exception) {
+            return $this->sendError($exception->getMessage(), "", 500);
+        }
+    }
+}
