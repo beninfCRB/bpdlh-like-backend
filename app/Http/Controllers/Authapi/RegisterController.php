369b886b4955ed311ff1f407f0f86d2c0276b834
Rifqi Arnoldy <rifqiarnoldy@gmail.com>
2024-11-25 23:30:34 +0700

    Update RegisterController.php

--- app/Http/Controllers/Authapi/RegisterController.php
+++ app/Http/Controllers/Authapi/RegisterController.php
@@ -130,9 +130,47 @@ class RegisterController extends ApiController
         }
     }
 
-    public function register_2(Register2Request $request): \Illuminate\Http\JsonResponse
+    public function register_2(Request $request): \Illuminate\Http\JsonResponse
     {
-        $input = $request->validated();
+        $validator = Validator::make($request->all(), [
+            'jenis_kelompok_masyarakat_id'  => 'required|exists:jenis_kelompok_masyarakats,id',
+            'kelompok_masyarakat'           => 'required',
+            'provinsi_kelompok_masyarakat_id'              => 'required',
+            'kabupaten_kelompok_masyarakat_id'             => 'required',
+            'kecamatan_kelompok_masyarakat_id'             => 'required',
+            'kelurahan_kelompok_masyarakat_id'             => 'required',
+            'profil_kelompok'               => 'required|file|mimes:pdf|max:2048',
+            'foto_ktp'                      => 'required|file|mimes:png,jpg,jpeg|max:2048',
+            'foto_selfie'                   => 'required|file|mimes:png,jpg,jpeg|max:2048',
+            'nama_pic'                  => 'required|max:255|string',
+            // 'nomor_identitas_pic'       => 'required|string|min:16|max:16|unique:data_pic_kelompok_masyarakats,nomor_identitas_pic',
+            'nomor_identitas_pic'       => ['required', 'string', 'min:16', 'max:16', \Illuminate\Validation\Rule::unique('data_pic_kelompok_masyarakats')->whereNull('deleted_at')],
+            'nomor_npwp_pic'            => 'nullable',
+            'alamat_pic'                => 'required|string|max:255',
+            'provinsi_pic'              => 'required',
+            'kabupaten_pic'             => 'required',
+            'kecamatan_pic'             => 'required',
+            'kelurahan_pic'             => 'required',
+            'tempat_lahir'              => 'required',
+            'tanggal_lahir'             => 'required|date',
+            'agama_id'                  => 'required|exists:agamas,id',
+            'status_perkawinan_id'      => 'required|exists:status_pernikahans,id',
+            'nama_gadis_ibu_kandung'    => 'required',
+            'jenis_pekerjaan_id'        => 'required|exists:jenis_pekerjaans,id',
+            'nohp_pic'                  => 'required|unique:data_pic_kelompok_masyarakats,nohp_pic',
+            // 'email_pic'                 => 'required|email|unique:data_pic_kelompok_masyarakats,email_pic|unique:user_akseslhs,email',
+            'email_pic'                 => ['required', 'email', \Illuminate\Validation\Rule::unique('data_pic_kelompok_masyarakats')->whereNull('deleted_at')],
+            'kode_aktivasi'             => 'required',
+        ]);
+
+        if ($validator->fails()) {
+            # code...
+            return $this->sendError(null, $validator->getMessageBag(), 422);
+        }
+
+        // Get all input
+        $input = $validator->validated();
+        // $input = $request->validated();
 
         $check_kelompok_masyarakat = \DB::table('kelompok_masyarakats')
             ->where('id', $input['kelompok_masyarakat'])
