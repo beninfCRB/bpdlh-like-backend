feb509675da246f68e688c0be5855b4b61e3f6eb
Rifqi Arnoldy <rifqiarnoldy@gmail.com>
2025-12-10 10:39:46 +0700

    Enhance TransaksiPenyaluranImport2 to log and notify on fund disbursement, and update email template for clarity

--- app/Imports/TransaksiPenyaluranImport2.php
+++ app/Imports/TransaksiPenyaluranImport2.php
@@ -2,10 +2,14 @@
 
 namespace App\Imports;
 
+use App\Jobs\TransaksiPenyaluranEmailJob;
 use App\Models\MasterDataBank;
 use App\Models\PengajuanKegiatan;
 use Illuminate\Support\Collection;
+use App\Models\LogTahapanPengajuanKegiatan;
 use Maatwebsite\Excel\Concerns\ToCollection;
+use App\Models\DetailLogTahapanPengajuanKegiatan;
+use App\Notifications\TransaksiPenyaluranNotification;
 
 class TransaksiPenyaluranImport2 implements ToCollection
 {
@@ -45,7 +49,47 @@ class TransaksiPenyaluranImport2 implements ToCollection
                         'nama_pemilik_rekening' => $item[4],
                         'tanggal_penyaluran'    => \Carbon\Carbon::instance(\PhpOffice\PhpSpreadsheet\Shared\Date::excelToDateTimeObject($item[5])), // Pastikan tanggal valid
                         'nilai_penyaluran'      => (float) $item[6],
+                        'flag'                  => 1,
+                        'username'              => auth()->user()->id,
                     ]);
+
+                    $log = LogTahapanPengajuanKegiatan::where('pengajuan_kegiatan_id', $pengajuan_kegiatan->id)
+                        ->whereHas('tahapan_pengajuan_kegiatan', function ($q) {
+                            $q->where('deskripsi_kegiatan', 'Konfirmasi Pencairan Dana Termin II');
+                        })
+                        ->first();
+
+                    $log->tanggal_selesai = date('Y-m-d');
+                    $log->user_akseslh_id = auth()->user()->id;
+                    $log->save();
+
+                    DetailLogTahapanPengajuanKegiatan::create([
+                        'pengajuan_kegiatan_id'         => $pengajuan_kegiatan->id,
+                        'tahapan_pengajuan_kegiatan_id' => $log->tahapan_pengajuan_kegiatan_id,
+                        'tanggal_masuk'                 => date("Y-m-d"),
+                        'tanggal_selesai'               => date("Y-m-d"),
+                        'user_akseslh_id'               => auth()->user()->id,
+                    ]);
+
+                    LogTahapanPengajuanKegiatan::where('pengajuan_kegiatan_id', $pengajuan_kegiatan->id)
+                        ->whereHas('tahapan_pengajuan_kegiatan', function ($q) {
+                            $q->where('deskripsi_kegiatan', 'Laporan Akhir Kegiatan');
+                        })
+                        ->update(['tanggal_masuk' => date("Y-m-d")]);
+
+                    $pengajuan_kegiatan->user_akseslh->unreadNotifications->markAsRead();
+
+                    $pengajuan_kegiatan->user_akseslh->notify(new TransaksiPenyaluranNotification($pengajuan_kegiatan->nomor_pengajuan, $pengajuan_kegiatan->user_akseslh->data_pic_kelompok_masyarakat->nama_pic, (float) $item[6]));
+
+                    $dataSend = [
+                        'nomor_pengajuan'   => $pengajuan_kegiatan->nomor_pengajuan,
+                        'nomor_rekening'    => (float) $item[6],
+                    ];
+
+                    TransaksiPenyaluranEmailJob::dispatch($pengajuan_kegiatan->user_akseslh, 'Pemberitahuan Pencairan Dana Termin II', $dataSend, null, 'mail.pencairan-dana-termin-2');
+
+                    $pengajuan_kegiatan->flag = 8;
+                    $pengajuan_kegiatan->save();
                 } else {
                     // Tambahkan log atau catatan jika bank tidak ditemukan
                     \Log::warning("Bank tidak ditemukan: " . $item[1]);
